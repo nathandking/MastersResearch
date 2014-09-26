5454c0bf975ce0659640948d782faae3c25874e6
nathank <nathank@sfu.ca>
2014-09-25 20:35:38 -0700

    built MDS into code working only on unique points for efficiency

--- Code/ClosestPointMethod/CPTextureMapping/test_visualization.m
+++ Code/ClosestPointMethod/CPTextureMapping/test_visualization.m
@@ -5,7 +5,7 @@ addpath('barylag2d');
 % 3D example on a sphere
 % Construct a grid in the embedding space
 
-dx = 0.2;                   % grid size
+dx = 0.1;                   % grid size
 
 % make vectors of x, y, positions of the grid
 x1d = (-2.0:dx:2.0)';
@@ -23,12 +23,11 @@ nz = length(z1d);
 % (cpx,cpy,cpz)
 
 % meshgrid is only needed for finding the closest points, not afterwards
-[xx yy zz] = meshgrid(x1d, y1d, z1d);
+[xx, yy, zz] = meshgrid(x1d, y1d, z1d);
 % function cpSphere for finding the closest points on a sphere
 [cpx, cpy, cpz, dist] = cpSphere(xx,yy,zz);
 % make into vectors
 cpxg = cpx(:); cpyg = cpy(:); cpzg = cpz(:);
-
 
 %% Banding: do calculation in a narrow band around the sphere
 dim = 3;    % dimension
@@ -44,33 +43,31 @@ band = find(abs(dist) <= bw*dx);
 cpxg = cpxg(band); cpyg = cpyg(band); cpzg = cpzg(band);
 xg = xx(band); yg = yy(band); zg = zz(band);
 
-
-%% Function u in the embedding space
-% u is a function defined on the grid (eg heat if solving the heat
-% equation)
-
-% assign some initial value (using initial value of cos (8*theta))
-%[th, phi, r] = cart2sph(xx,yy,zz);
-%u = cos(phi + pi/2);
-
-% this makes u into a vector, containing only points in the band
-u1 = cpxg;
-u2 = cpyg;
-u3 = cpzg;
-
-P = [u1, u2, u3];
+%% Find unique points to apply MDS.
+P = [cpxg, cpyg, cpzg];
 
 [~, ia, ~] = unique(P,'first','rows');
 ia = sort(ia);
-P = P(ia,:);
 
-load('dx0p2/newx.mat');
-load('dx0p2/newy.mat');
+cpxg_unique = P(ia,1);
+cpyg_unique = P(ia,2);
+cpzg_unique = P(ia,3);
+
+u1 = cpxg_unique;
+u2 = cpyg_unique;
+u3 = cpzg_unique;
+
+%% Apply MDS to unique list.
+[newx, newy] = MDS(u1,u2,u3);
+
+%%
+% load('dx0p2/newx.mat');
+% load('dx0p2/newy.mat');
 uvColor = imread('BlurredThreeShapes512.jpg');
 %uvColor = checkerboard(10,2,2);
-
-newx = newx(band); newy = newy(band);
-newx = newx(ia); newy = newy(ia);
+% 
+% newx = newx(band); newy = newy(band);
+% newx = newx(ia); newy = newy(ia);
 xmin = min(newx); ymin = min(newy);
 xmax = max(newx); ymax = max(newy);
 
@@ -86,20 +83,20 @@ UV = [uu(:),vv(:)];
 newXY = [newx, newy];
 k = dsearchn(UV,newXY);
 p = 0;
-%Ui = uvColor(k);
-uvC = uvColor(:);
-for i = 1:length(k)
-Ui(i) = barylag2d(double(uvC(k(i)-p:k(i)+p)),...
-    uu(k(i)-p:k(i)+p),vv(k(i)-p:k(i)+p),newx(i),newy(i));
-end
+Ui = uvColor(k);
+%uvC = uvColor(:);
+% for i = 1:length(k)
+% Ui(i) = barylag2d(double(uvC(k(i)-p:k(i)+p)),...
+%     uu(k(i)-p:k(i)+p),vv(k(i)-p:k(i)+p),newx(i),newy(i));
+% end
 
 figure;
 imagesc(Ui)
 
-DT = delaunayTriangulation(u1(ia),u2(ia),u3(ia));
+DT = delaunayTriangulation(u1,u2,u3);
 Tri = freeBoundary(DT);
 figure;
-trisurf(Tri,u1(ia),u2(ia),u3(ia),Ui);
-figure;
-scatter3(u1(ia),u2(ia),u3(ia),20,Ui)
+trisurf(Tri,u1,u2,u3,Ui);
+% figure;
+% scatter3(u1(ia),u2(ia),u3(ia),20,Ui)
 
