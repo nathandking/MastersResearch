4ff19002c4ec01b8a6a2908c57b57ae99aaf934b
nathank <nathank@sfu.ca>
2014-10-05 15:31:25 -0700

    changed code to work for precomputed initial maps

--- Code/ClosestPointMethod/CPTextureMapping/test_visualization.m
+++ Code/ClosestPointMethod/CPTextureMapping/test_visualization.m
@@ -1,114 +1,23 @@
-restoredefaultpath;
-addpath('cp_matrices_Sphere');
-addpath('barylag2d');
-%%
-% 3D example on a sphere
-% Construct a grid in the embedding space
-tic
-dx = 0.2;                   % grid size
+clear all;
+close all;
+%% load initial mapping of image onto sphere.
 
-% make vectors of x, y, positions of the grid
-x1d = (-2.0:dx:2.0)';
-y1d = x1d;
-z1d = x1d;
+load('InitialMaps/SphereMDS41.mat');
+W = double(U);
 
-nx = length(x1d);
-ny = length(y1d);
-nz = length(z1d);
-
-
-
-%% Find closest points on the surface
-% For each point (x,y,z), we store the closest point on the sphere
-% (cpx,cpy,cpz)
-
-% meshgrid is only needed for finding the closest points, not afterwards
-[xx, yy, zz] = meshgrid(x1d, y1d, z1d);
-% function cpSphere for finding the closest points on a sphere
-[cpx, cpy, cpz, dist] = cpSphere(xx,yy,zz);
-% make into vectors
-cpxg = cpx(:); cpyg = cpy(:); cpzg = cpz(:);
-
-%% Banding: do calculation in a narrow band around the sphere
-dim = 3;    % dimension
-p = 3;      % interpolation degree
-order = 2;  % Laplacian order
-% "band" is a vector of the indices of the points in the computation
-% band.  The formula for bw is found in [Ruuth & Merriman 2008] and
-% the 1.0001 is a safety factor.
-bw = 1.0001*sqrt((dim-1)*((p+1)/2)^2 + ((order/2+(p+1)/2)^2));
-band = find(abs(dist) <= bw*dx);
-
-% store closest points in the band;
-cpxg = cpxg(band); cpyg = cpyg(band); cpzg = cpzg(band);
-xg = xx(band); yg = yy(band); zg = zz(band);
-
-%% Find unique points to apply MDS.
-P = [cpxg, cpyg, cpzg];
-
-[~, ia, ~] = unique(P,'first','rows');
-ia = sort(ia);
-
-cpxg_unique = P(ia,1);
-cpyg_unique = P(ia,2);
-cpzg_unique = P(ia,3);
-
-u1 = cpxg_unique;
-u2 = cpyg_unique;
-u3 = cpzg_unique;
-
-%% Apply MDS to unique list.
-% Compute geodesic distance between pairs of points.
-% This below only works for a sphere!
-A = [u1, u2, u3];
-NumPts = length(u1);
-M = zeros(NumPts);
-for j = 1:NumPts
-    for i = 1:NumPts
-        M(i,j) = (real(acos(sum(A(i,:,:).*A(j,:,:))))).^2;
-    end
-end
-
-[newx, newy] = MDS(M);
-toc
-%%
-% load('dx0p2/newx.mat');
-% load('dx0p2/newy.mat');
-uvColor = imread('BlurredThreeShapes512.jpg');
-%uvColor = checkerboard(10,2,2);
-% 
-% newx = newx(band); newy = newy(band);
-% newx = newx(ia); newy = newy(ia);
-xmin = min(newx); ymin = min(newy);
-xmax = max(newx); ymax = max(newy);
-
-u = (xmin:(xmax-xmin)/(size(uvColor,1)-1):xmax)';
-v = (ymin:(ymax-ymin)/(size(uvColor,2)-1):ymax)';
-
-[uu,vv]=meshgrid(u,v);
+%% initial planar image.
 figure;
-mesh(uu,vv,uvColor);
+PlanarImage = reshape(W,size(xS));
+imagesc(PlanarImage)
 
-%find indices of the closest points in (u,v) to (newx,newy).
-UV = [uu(:),vv(:)];
-newXY = [newx, newy];
-k = dsearchn(UV,newXY);
-p = 0;
-uvC = uvColor(:);
-Ui = uvC(k);
-%uvC = uvColor(:);
-% for i = 1:length(k)
-% Ui(i) = barylag2d(double(uvC(k(i)-p:k(i)+p)),...
-%     uu(k(i)-p:k(i)+p),vv(k(i)-p:k(i)+p),newx(i),newy(i));
-% end
-
-figure;
-imagesc(Ui)
-
-DT = delaunayTriangulation(u1,u2,u3);
+%% triangulation of surface using Delaunay triangulation.
+xSg = xS(:); ySg = yS(:); zSg = zS(:);
+DT = delaunayTriangulation(xSg,ySg,zSg);
 Tri = freeBoundary(DT);
 figure;
-trisurf(Tri,u1,u2,u3,Ui);
- figure;
- scatter3(u1,u2,u3,20,Ui)
+trisurf(Tri,xSg,ySg,zSg,W,'EdgeColor','none');
+
+% %% scatter plot of the points with color.
+% figure;
+% scatter3(xSg,ySg,zSg,20,W,'fill')
 
